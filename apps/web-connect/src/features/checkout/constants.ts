bedefb9ad2215e420974de8486c263644815f22a
Stanyslav P <sp@cryptit.at>
2024-06-26 17:35:42 +0300

    improved constants for checkout page

--- apps/web-connect/src/features/checkout/constants.ts
+++ apps/web-connect/src/features/checkout/constants.ts
@@ -7,16 +7,15 @@ export const STAKING_DEV_URL = "https://xai-staking-int.dev.cryptit.at/";
 export const STAKING_LOCAL_URL = "http://localhost:3000/";
 export const STAKING_PROD_URL = "https://app.xai.games/"
 
-//todo improve constants
 
 export interface IRedirects {
-    "http://localhost:5173/sentry": string;
-    "https://cryptitaustria.github.io/sentry/": string;
-    "https://sentry.xai.games/": string;
+    [SENTRY_LOCAL_URL]: string;
+    [SENTRY_DEV_URL]: string;
+    [SENTRY_PROD_URL]: string;
 }
 
 export const redirects: IRedirects = {
-    "http://localhost:5173/sentry": STAKING_LOCAL_URL,
-    "https://cryptitaustria.github.io/sentry/": STAKING_DEV_URL,
-    "https://sentry.xai.games/": STAKING_PROD_URL
+    [SENTRY_LOCAL_URL]: STAKING_LOCAL_URL,
+    [SENTRY_DEV_URL]: STAKING_DEV_URL,
+    [SENTRY_PROD_URL]: STAKING_PROD_URL
 }
